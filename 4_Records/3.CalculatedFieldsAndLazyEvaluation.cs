7f51740ac24d8c5d4a9fd0ec7910e21c169e86ff
m.hosseini <m.hosseini@xaniis.com>
2024-06-08 13:28:49 +0330

    add new file

--- 4_Records/3.CalculatedFieldsAndLazyEvaluation.cs
+++ 4_Records/3.CalculatedFieldsAndLazyEvaluation.cs
@@ -1,42 +1,55 @@
 namespace CSharp12InANutshell.Records;
 
-//one problem:
-//1. recomputing DistanceFromOrigin every time the property is accessed
-// record Point(double X, double Y)
-// {
-//     //returns the distance from the origin (0, 0):
-//     public double DistanceFromOrigin => Math.Sqrt(X * X + Y * Y);
-// }
+////one problem:
+////1. recomputing DistanceFromOrigin every time the property is accessed
+record Point1(double X, double Y)
+{
+    //returns the distance from the origin (0, 0):
+    public double DistanceFromOrigin => Math.Sqrt(X * X + Y * Y);
+}
 
 
-//two problem:
-//1. it doesn’t allow for nondestructive mutation (changing X and Y to init-only properties would break the code because DistanceFromOrigin
-//would become stale after the init accessors execute).
+////two problem:
+////1. it doesn’t allow for nondestructive mutation (changing X and Y to init-only properties would break the code because DistanceFromOrigin
+////would become stale after the init accessors execute).
 
-//2. It’s also suboptimal in that the calculation is always performed, regardless of whether the DistanceFromOrigin property is ever read.
-// record Point
-// {
-//     public Point(double x, double y) => (X, Y, DistanceFromOrigin) = (x, y, Math.Sqrt(x * x + y * y));
+////2. It’s also suboptimal in that the calculation is always performed, regardless of whether the DistanceFromOrigin property is ever read.
+record Point2
+{
+    public Point2(double x, double y) => (X, Y, DistanceFromOrigin) = (x, y, Math.Sqrt(x * x + y * y));
 
-//     public double X { get;}
-//     public double Y { get;}
-//     public double DistanceFromOrigin { get; }
-// }
+    public double X { get; }
+    public double Y { get; }
+    public double DistanceFromOrigin { get; }
+}
+
+record Point2_1
+{
+    public Point2_1(double x, double y) => (X, Y, DistanceFromOrigin) = (x, y, Math.Sqrt(x * x + y * y));
+
+    public double X { get; init; }
+    public double Y { get; init; }
+    public double DistanceFromOrigin { get; }
+}
+
+
 
 //The optimal solution is to cache its value in a field and populate it lazily (on first use):
-
-record Point(double X, double Y)
+record Point3
 {
-    double? _distanceFromOrigin;
-    // public double DistanceFromOrigin
-    // {
-    //     get
-    //     {
-    //         if (_distanceFromOrigin == null)
-    //             _distanceFromOrigin = Math.Sqrt(X * X + Y * Y);
-
-    //         return _distanceFromOrigin.Value;
-    //     }
-    // }
-    public double DistanceFromOrigin => _distanceFromOrigin ??= Math.Sqrt(X * X + Y * Y);
+    public Point3(double x, double y) => (X, Y) = (x, y);
+    double _x, _y;
+    public double X
+    {
+        get => _x; init { _x = value; _distance = null; }
+    }
+    public double Y
+    {
+        get => _y; init { _y = value; _distance = null; }
+    }
+    double? _distance;
+    public double DistanceFromOrigin => _distance ??= Math.Sqrt(X * X +
+    Y * Y);
 }
+
+
--- 4_Records/Program.cs
+++ 4_Records/Program.cs
@@ -6,20 +6,28 @@ public class Program
         // Point p1 = new Point(2, 3);
         // Point p2 = p1 with { X = double.NaN }; // throws an exception
 
-        //recomputing DistanceFromOrigin every time the property is accessed
-        // Point p1 = new Point(2, 3);
-        // Console.WriteLine(p1.DistanceFromOrigin);
-        // Console.WriteLine(p1.DistanceFromOrigin);
-        // Console.WriteLine(p1.DistanceFromOrigin);
+        /////recomputing DistanceFromOrigin every time the property is accessed
+        Point1 p1 = new Point1(2, 3);
+        Console.WriteLine(p1.DistanceFromOrigin);
+        Console.WriteLine(p1.DistanceFromOrigin);
+        Console.WriteLine(p1.DistanceFromOrigin);
+
+        Point2 p2 = new Point2(2, 3);
+        Console.WriteLine(p2.DistanceFromOrigin);
+        Console.WriteLine(p2.DistanceFromOrigin);
+        ///Point2 p6 = p2 with {X = 1};  //error
+
+        Point2_1 p3 = new Point2_1(2, 3);
+        Console.WriteLine(p3.DistanceFromOrigin);
+        Point2_1 p4 = p3 with {X = 1};
+        Console.WriteLine(p4.DistanceFromOrigin);
 
         // Point p1 = new Point(2, 3);
-        // Console.WriteLine(p1.DistanceFromOrigin);
-        // Point p2 = p1 with {X = double.NaN};
+        // Console.WriteLine(p1.DistanceFromOrigin); // 3.605551275463989
+        // Point p2 = p1 with { Y = 4 };
+        // Console.WriteLine(p2.DistanceFromOrigin); // 4.47213595499958
 
-        // Point p1 = new Point(2, 3);
-        // Console.WriteLine(p1.DistanceFromOrigin);
-        // Point p2 = p1 with {X = 1};
-        // Console.WriteLine(p2.DistanceFromOrigin);
+
 
     }
 }
